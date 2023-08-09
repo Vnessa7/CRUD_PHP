3093c216dd617b5982c2287eb30918a2e0d8c8aa
VanessaGarcia <vanessa_garciad@hotmail.com>
2023-08-09 18:12:40 -0500

    avance 9/8

---
+++ config/conexion.php
@@ -0,0 +1,17 @@
+<?php
+    class Conectar{
+        protected $dbh;
+
+        protected function Conexion(){
+                try{
+                    $conectar = $this->dbh = new PDO("mysql:localhost;dbname=crud","root","");
+                    return $conectar;
+                }catch(Exception $e){
+                    print "¡Error BD!:" .  $e->getMessage() . "/<br>";
+                }
+        }
+        public function set_names(){
+            return $this->dbh->query("SET NAMES 'utf-8'")
+        }
+    }
+?>
